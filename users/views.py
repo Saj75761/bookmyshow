2b548816af4cce6d93e9e5dfa1a614ddbdd5721e
Santosh <ashokjodatti@gmail.com.com>
2026-06-20 12:46:23 +0530

    Fix user registration Form field error

--- users/views.py
+++ users/views.py
@@ -12,11 +12,8 @@ def register(request):
     if request.method == 'POST':
         form=UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            username=form.cleaned_data.get('username')
-            password=form.cleaned_data.get('password1')
-            user=authenticate(username=username,password=password)
-            login(request,user)
+            user = form.save()
+            login(request, user)
             return redirect('profile')
     else:
         form=UserRegisterForm()
